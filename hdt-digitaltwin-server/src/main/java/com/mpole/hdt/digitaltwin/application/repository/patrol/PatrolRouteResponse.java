5be61c8970367737a9aecea73d6e21bfafc264a4
hellotect2022go <hellotect2022.go@gmail.com>
2026-02-23 17:08:39 +0900

    [FEATURE] PATROL 가상순찰 관련 테이블 생성 및 단순 조회 API 작성

---
+++ hdt-digitaltwin-server/src/main/java/com/mpole/hdt/digitaltwin/application/repository/patrol/PatrolRouteResponse.java
@@ -0,0 +1,16 @@
+package com.mpole.hdt.digitaltwin.application.repository.patrol;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatrolRouteResponse {
+    private Long routeId;
+    private String routeName;
+    private String description;
+    private boolean active;
+    private Integer waypointCount;
+}
